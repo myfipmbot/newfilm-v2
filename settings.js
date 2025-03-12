f8c19be272cadb1cc0b16af9bdda1586713a1d0f
𝗞𝗔𝗩𝗜-𝗘𝗫𝗘 𝗢𝗪𝗡𝗘𝗥 👨‍💻💗 <tharushankavishka02@gmail.com>
2025-03-13 02:16:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,6 @@ OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94760383959' : process.e
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
-MAX_SIZE: 500,
-STATUS_REPLY_MESSAGE: '𝐘𝐎𝐔𝐑 𝐒𝐓𝐀𝐓𝐔𝐒 𝐒𝐄𝐄𝐍 𝐉𝐔𝐒𝐓 𝐍𝐎𝐖 𝐁𝐘 𝐕𝐀𝐉𝐈𝐑𝐀 𝐌𝐃 ✅',    
-ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
+MAX_SIZE: 500,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
